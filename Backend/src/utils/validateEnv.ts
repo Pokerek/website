4616c976d04d5ed79f0c02847ac92a062fd2fc10
Pokerek <karolchrobok@gmail.com>
2023-05-31 13:51:00 +0200

    Update configuration for validate .env

--- Backend/src/utils/validateEnv.ts
+++ Backend/src/utils/validateEnv.ts
@@ -1,4 +1,4 @@
-import { cleanEnv, str, port } from 'envalid';
+import { cleanEnv, str, port, email } from 'envalid';
 
 const validateEnv = () => {
   cleanEnv(process.env, {
@@ -6,7 +6,9 @@ const validateEnv = () => {
     DB_PASSWORD: str(),
     DB_PATH: str(),
     PORT: port(),
-    JWT_SECRET: str()
+    JWT_SECRET: str(),
+    EMAIL: email(),
+    EMAIL_PASSWORD: str()
   });
 };
 
