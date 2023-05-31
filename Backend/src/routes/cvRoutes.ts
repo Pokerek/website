7e2da0594d5081966ed4f50f153f733e7a52c9d9
Pokerek <karolchrobok@gmail.com>
2023-05-31 13:22:18 +0200

    Add configuration file for multer storage

--- Backend/src/routes/cvRoutes.ts
+++ Backend/src/routes/cvRoutes.ts
@@ -1,23 +1,14 @@
 import { Router } from 'express';
 import CVController from '../controllers/cvController';
 import RouterWithPath from '../types/router';
-import multer from 'multer';
 import authMiddleware from '../middleware/authMiddleware';
+import uploadCV from '../config/multer';
 
 class CVRoutes implements RouterWithPath {
   public path = '/cv';
   public router = Router();
 
   private cvController = new CVController();
-  private storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-      cb(null, 'cv.pdf');
-    }
-  });
-  private upload = multer({ storage: this.storage });
 
   constructor() {
     this.initializeRoutes();
@@ -28,7 +19,7 @@ class CVRoutes implements RouterWithPath {
     this.router.post(
       this.path,
       authMiddleware,
-      this.upload.single('cv'),
+      uploadCV.single('cv'),
       this.cvController.updateCV
     );
   }
