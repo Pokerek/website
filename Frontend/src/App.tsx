1bb5301535fa9bbccd8c24ccb94f80b7f955e445
Pokerek <karolchrobok@gmail.com>
2023-06-04 14:58:22 +0200

    Fix issue with react-router-dom

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { Routes, Route, useLocation } from "react-router-dom";
 import { useContext, useEffect } from "react";
 
 import { Footer } from "./components/Layout/footer/Footer";
@@ -15,38 +14,9 @@ import { AuthContext } from "./context/AuthContext";
 import { WriteContext } from "./context/WriteContext";
 
 function App() {
-  const Authentication = useContext(AuthContext);
-  const Write = useContext(WriteContext);
-
-  // const { pathname } = useLocation();
-
-  // useEffect(() => {
-  //   if (pathname !== "/admin/edit" && Write) {
-  //     Write.handlePostChange(null);
-  //   }
-  // }, [pathname]);
-
   return (
-    <div className="App" onKeyDown={Authentication?.handleKeyPress}>
+    <div className="App">
       <Header />
-      {/* <Container>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Welcome />} />
-            <Route path="about.me" element={<About />} />
-            <Route path="journal.dev" element={<Journal />} />
-            <Route path="project.file" element={<ProjectList />} />
-            {Authentication?.isAdmin && (
-              <Route path="auth" element={<LoginForm />} />
-            )}
-            {Authentication?.isLogin && (
-              <Route path="edit" element={<WritePost />} />
-            )}
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        </Routes>
-      </Container>
-      <Footer /> */}
     </div>
   );
 }
