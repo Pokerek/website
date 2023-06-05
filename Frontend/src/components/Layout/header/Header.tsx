fdc3ce9e3f9cf578e3fe9e224fa6a2a1c98e8570
Pokerek <karolchrobok@gmail.com>
2023-06-05 12:18:52 +0200

    Change header style Add sticky to header style

--- Frontend/src/components/Layout/header/Header.tsx
+++ Frontend/src/components/Layout/header/Header.tsx
@@ -6,11 +6,13 @@ import { Button } from "../../custom/Button/Button";
 export const Header = () => {
   return (
     <header className="header">
-      <a className="header__logo">Chrobok</a>
-      <Button href="#contact" className="header__button">
-        Get in touch
-      </Button>
-      <Navigation />
+      <div className="header__inner">
+        <a className="header__logo">Chrobok</a>
+        <Button href="#contact" className="header__button">
+          Get in touch
+        </Button>
+        <Navigation />
+      </div>
     </header>
   );
 };
