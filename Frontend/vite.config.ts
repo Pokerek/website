a20bea134001f13da557a6a841ccd8c9a7c085ab
Karol Chrobok <karolchrobok@gmail.com>
2023-07-20 00:24:58 +0200

    Try with vite.config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', "react-routed-dom"],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
 })
