2a161e3eaa2b3fb65f4b941f4f5dace96a680344
Karol Chrobok <karolchrobok@gmail.com>
2023-07-19 21:44:53 +0200

    Add secure to set-Cookie response

--- Backend/src/controllers/authentication-controller.ts
+++ Backend/src/controllers/authentication-controller.ts
@@ -20,6 +20,7 @@ export default class AuthenticationController {
                     httpOnly: true,
                     expires: response.expiresAt,
                     sameSite: 'none',
+                    secure: true,
                 }
             ).send(
                 {
